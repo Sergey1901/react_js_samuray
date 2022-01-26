8810eeb384dd577053724f42b207bbad047c0326
Sergey <dor_serg91@mail.ru>
2022-01-26 15:37:57 +0300

    'samuray_11'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <header className="header">
+     <img src='https://static4.depositphotos.com/1000423/454/i/600/depositphotos_4548401-stock-photo-symbol-of-yin-and-yang.jpg'/>
+   </header>
+        <nav className='nav'>
+            <div><a>Profile</a></div>
+            <div><a>Messages</a></div>
+            <div><a>News</a></div>
+            <div><a>Music</a></div>
+            <div><a>Settings</a></div>
+        </nav>
+        <div className='context'>
+            <img src='https://klike.net/uploads/posts/2019-06/1561182942_2.jpg'/>
+             <div>ava + description</div>
+            <div>
+                Post
+                <div>newPost</div>
+            </div>
+            <div>post1</div>
+            <div>post2</div>
+            <div>post3</div>
+        </div>
+
+
     </div>
   );
 }
